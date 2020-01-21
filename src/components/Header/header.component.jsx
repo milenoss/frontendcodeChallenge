e79cdb049cd187a4dd03ef39d63954ea75d6b7e9
milenoss <milenkhanal@gmail.com>
2020-01-21 21:39:33 +0000

    header styling done

--- src/components/Header/header.component.jsx
+++ src/components/Header/header.component.jsx
@@ -3,13 +3,15 @@ import F1 from '../../assets/images/nav_logo.png'
 
 const Header = () => ( 
     
-      <div className ='header'> 
-       <img src={F1} alt = 'F1'></img>
-       <h3>HOME</h3>
-       <h3>FEATURES</h3>
-       <h3>NEWS</h3>
-       <h3>BUY NOW</h3>
-
+      <div className ='header'>
+      <div className ='F1container'>
+       <img src={F1} alt = 'F1'></img></div> 
+       <div className = 'opt'>
+       <p>HOME</p>
+       <p>FEATURES</p>
+       <p>NEWS</p>
+       <p>BUY NOW</p>
+       </div>
        </div>
       
     
